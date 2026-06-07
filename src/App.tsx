e6f6c0b38686b7f575fb398709948f72fecc3c8b
Martin Fischer <mamficher92@gmail.com>
2026-06-07 08:48:51 +0200

    chore: set up router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Route, Routes } from 'react-router';
 import Home from './components/pages/Home.tsx';
+import ProjectDetails from './components/pages/ProjectDetails.tsx';
 
 export default function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects/:id" element={<ProjectDetails />} />
+    </Routes>
+  );
 }
